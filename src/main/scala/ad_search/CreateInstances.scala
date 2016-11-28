f28fb298ccf587c704122d7e6d6bce09199a02fe
duanxiping <cicido@126.com>
2016-11-28 22:55:28 +0800

    1. 优化代码，在使用withColumn时保持原列名

--- src/main/scala/ad_search/CreateInstances.scala
+++ src/main/scala/ad_search/CreateInstances.scala
@@ -3,10 +3,9 @@ package ad_search
 import java.util.Calendar
 
 import org.apache.spark.ml.feature.VectorAssembler
-import org.apache.spark.mllib.linalg.{Vector, Vectors}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.hive.HiveContext
-import org.apache.spark.sql.types.{BooleanType, IntegerType, LongType, StringType}
+import org.apache.spark.sql.types._
 import org.apache.spark.sql.{DataFrame, SQLContext}
 import org.apache.spark.{SparkConf, SparkContext}
 
@@ -44,12 +43,24 @@ object CreateInstances {
     val outEventTable = "ad_recommend.dxp_ad_search_event_data"
     val ModuelFile = "ad_search_model"
 
+
     val imeiDF = getImeiData(imeiTable)
-    saveDataFrame(imeiDF,outImeiTable,dt)
+    saveDataFrame(imeiDF, outImeiTable, dt)
     val appDF =  getAppData(appTable,dt)
     saveDataFrame(appDF,outAppTable,dt)
     val eventDF = getEventData(eventTable,dt)
     saveDataFrame(eventDF, outEventTable,dt)
+
+    val imeiCols = imeiDF.columns.filter(_ != "imei")
+    val appCols = appDF.columns.filter(_ != "appid")
+    val label = "oper_type"
+    val eventCols = eventDF.columns.filter(!List("imei","appid",label).contains(_))
+    val allCols = imeiCols ++ appCols ++ eventCols
+
+    val mergeDF = eventDF.join(imeiDF,"imei").join(appDF, "appid").select(label, allCols:_*)
+
+    //one hot
+
   }
 
   def makeTestData(): DataFrame = {
@@ -68,36 +79,45 @@ object CreateInstances {
     val stringToDouble = udf[Double, String] {
       catMap(_)
     }
-    df.withColumn(col + "_cat", stringToDouble(df(col)))
-    df.drop(col)
+    df.withColumn(col, stringToDouble(df(col)))
   }
 
-  //one-hot编码
+  /*
   def oneColProcessWithOneHot(col: String) = (df: DataFrame) => {
-    val sma = df.schema
-    val catMap = sma(col).dataType match {
-      case IntegerType => df.select(col).distinct.map(_.getAs[Int](col).toString).collect.zipWithIndex.toMap
-      case LongType => df.select(col).distinct.map(_.getAs[Long](col).toString).collect.zipWithIndex.toMap
-      case StringType => df.select(col).distinct.map(_.getAs[String](col)).collect.zipWithIndex.toMap
-      case BooleanType => df.select(col).distinct.map(_.getAs[Boolean](col).toString).collect.zipWithIndex.toMap
-    }
+    val catMap = df.select(col).distinct.map(_.getAs[String](col).toString).collect.zipWithIndex.toMap
     println(catMap)
 
-    val typeToVector = sma(col).dataType match {
-      case IntegerType => udf[Vector, Int] { w =>
-        Vectors.sparse(catMap.size, Array(catMap(w.toString)), Array(1))
-      }
-      case LongType => udf[Vector, Long] { w =>
-        Vectors.sparse(catMap.size, Array(catMap(w.toString)), Array(1))
-      }
-      case StringType => udf[Vector, String] { w =>
-        Vectors.sparse(catMap.size, Array(catMap(w)), Array(1))
-      }
-      case BooleanType => udf[Vector, Boolean] { w =>
-      Vectors.sparse(catMap.size, Array(catMap(w.toString)), Array(1))
+    val stringToVector =  udf[Vector, String] { w =>
+      Vectors.sparse(catMap.size, Array(catMap(w)), Array(1))
     }
-    df.withColumn(col + "_cat", typeToVector(df(col)))
-    df.drop(col)
+    val new_df = df.withColumn(col + "_cat", stringToVector(df(col)))
+    new_df.drop(col)
+  }
+  */
+
+  def convertToString(col: String) = (df: DataFrame) => {
+    val sma = df.schema
+
+    sma(col).dataType match {
+      case StringType => df
+      case _ => {
+        val anyToString = sma(col).dataType match {
+          case IntegerType => udf[String, Int] {
+            _.toString
+          }
+          case LongType => udf[String, Long] {
+            _.toString
+          }
+          case BooleanType => udf[String, Boolean] {
+            _.toString
+          }
+          case DoubleType => udf[String, Double] {
+            _.toString
+          }
+        }
+        df.withColumn(col, anyToString(df(col)))
+      }
+    }
   }
 
   //对于非cat类型的字段进行分段处理
@@ -105,30 +125,24 @@ object CreateInstances {
     //分段范围计算
     val splitToDouble = udf[Double, Double] { w =>
       var i = 0
-      while (i < colRange.length && w < colRange(i)) {
+      while (i < colRange.length && w > colRange(i)) {
         i += 1
       }
       i.toDouble
     }
-    df.withColumn(col + "_split", splitToDouble(df(col)))
+    df.withColumn(col, splitToDouble(df(col)))
   }
 
-
-  def multiColProcess(df: DataFrame, label: String, needOneHot: Boolean): DataFrame = {
+  def multiColProcess(df: DataFrame, label: String): DataFrame = {
     val cols = df.columns.filter(_ != label)
     var new_df = df
-    if (needOneHot) {
-      for (col <- cols) {
-        new_df = oneColProcessWithOneHot(col)(new_df)
-      }
-    } else {
-      for (col <- cols) {
-        new_df = oneColProcess(col)(new_df)
-      }
+    for (col <- cols) {
+      new_df = convertToString(col)(new_df)
+      new_df = oneColProcess(col)(new_df)
+      println(new_df.columns)
     }
     new_df
   }
-
 
   //get train data
   def assembleFeatures(df: DataFrame, label: String): DataFrame = {
@@ -139,21 +153,23 @@ object CreateInstances {
     assembler.transform(df).select(label, "features")
   }
 
-  def saveDataFrame(df:DataFrame,outTable:String,dt:Long): Unit ={
+  def saveDataFrame(df: DataFrame, outTable: String, dt: Long): Unit = {
     val cols = df.columns
     val sma = df.schema
-    val colsType = cols.map(r=>{
+    val colsType = cols.map(r => {
       sma(r).dataType match {
         case IntegerType => "int"
         case LongType => "bigint"
         case StringType => "string"
         case BooleanType => "boolean"
+        case DoubleType => "double"
       }
     })
 
-    val colsString = cols.zip(colsType).map(r=>r._1 + " " + r._2).mkString(",")
+    val colsString = cols.zip(colsType).map(r => r._1 + " " + r._2).mkString(",")
     val create_table_sql: String = s"create table if not exists $outTable " +
       s" ($colsString) partitioned by (stat_date bigint) stored as textfile"
+    println(create_table_sql)
     hiveContext.sql(create_table_sql)
 
     val tmptable = "dxp_tmp_table"
@@ -164,6 +180,7 @@ object CreateInstances {
     hiveContext.sql(insert_sql)
     hiveContext.dropTempTable(tmptable)
   }
+
   //get imei data
   //tbname:user_profile.idl_fdt_dw_tag
   def getImeiData(tbname: String): DataFrame = {
@@ -175,8 +192,9 @@ object CreateInstances {
     val label = "imei"
     val cols = Array("user_age", "sex", "user_job", "marriage_status", "mz_apps_car_owner", "user_network_type")
     val colsString = label + "," + cols.mkString(",")
-    val selectSQL = s"select $colsString from $tbname"
-    multiColProcess(hiveContext.sql(selectSQL), label, true)
+    val selectSQL = s"select $colsString from $tbname limit 10000"
+    println(selectSQL)
+    multiColProcess(hiveContext.sql(selectSQL), label)
   }
 
   //get app data
@@ -185,8 +203,9 @@ object CreateInstances {
     val label = "appid"
     val catCols = Array("fcategoryid", "fcategory2id")
     val splitCols = Array("fevaluate_count", "fdownload_count", "fstars", "finstall_count", "appsize")
-    val splitRanges: Array[Array[Long]] = Array(Array(100, 1000, 10000, 100000), Array(1000, 10000, 100000, 1000000),
-      Array(1000, 10000, 100000, 1000000), Array(1000, 10000, 100000), Array(102400, 1024000, 1024000, 10240000))
+    val stdArray:Array[Long] = Array(1,10,100)
+    val splitRanges: Array[Array[Long]] = Array(stdArray.map(_*10), stdArray.map(_*1000),
+      stdArray.map(_*1000), stdArray.map(_*1000), Array(10240, 102400, 102400, 1024000))
 
     val colsString = (Array(label) ++ catCols ++ splitCols).mkString(",")
     val selectSQL = s"select $colsString from $tbname where stat_date=$dt"
@@ -194,18 +213,9 @@ object CreateInstances {
 
     val splitColsRanges = splitCols.zip(splitRanges)
     for (col <- splitColsRanges) {
-      oneColProcessWithSplit(col._1, col._2)(df)
-      df.drop(col._1)
+      df = oneColProcessWithSplit(col._1, col._2)(df)
     }
-
-    val resSplitCols = splitCols.map(_ + "_split")
-    val cols = catCols ++ resSplitCols
-
-    for (col <- cols) {
-      oneColProcessWithOneHot(col)(df)
-      df.drop(col)
-    }
-    df
+    multiColProcess(df,label)
   }
 
   //get event data
@@ -235,20 +245,19 @@ object CreateInstances {
     val getHour = udf[Double, Long] { w =>
       cal.setTimeInMillis(w)
       cal.get(Calendar.HOUR_OF_DAY) match {
-        case i if List(2,3,4,5,6,7).contains(i) => 0
-        case i if List(8,9,10,11,12,13).contains(i) => 1
-        case i if List(14,15,16,17,18,19).contains(i) => 2
-        case i if List(20,21,22,23,0,1).contains(i) => 3
+        case i if List(2, 3, 4, 5, 6, 7).contains(i) => 0
+        case i if List(8, 9, 10, 11, 12, 13).contains(i) => 1
+        case i if List(14, 15, 16, 17, 18, 19).contains(i) => 2
+        case i if List(20, 21, 22, 23, 0, 1).contains(i) => 3
         case _ => 4
       }
     }
 
     var df = hiveContext.sql(selectSQL)
-    df.withColumn("oper_type_cat", stringToDouble(df("oper_type")))
-    df.drop("oper_type")
+    df = df.withColumn("oper_type", stringToDouble(df("oper_type")))
 
-    df.withColumn("day_of_week",getWeek(df("oper_time")))
-    df.withColumn("hour_of_day",getHour(df("oper_time")))
+    df = df.withColumn("day_of_week", getWeek(df("oper_time")))
+    df = df.withColumn("hour_of_day", getHour(df("oper_time")))
     df.drop("oper_time")
   }
 }
